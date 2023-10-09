07d3712d0c93a09a7db83e773ef948ca59b6383c
Ozenc Kandemir <ozenckandemir@hotmail.com>
2023-10-09 15:51:14 +0300

    comment added

--- app/server.py
+++ app/server.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 
 @app.route('/<random_string>')
 def return_backwards_string(random_string):
+    # comment test
     return "".join(reversed(random_string))
 
 @app.route('/get-project')
